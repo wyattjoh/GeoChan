ca701756b0645862baeb871314bcf9083aba9be8
bvincent1 <vincent@ualberta.ca>
2014-03-01 13:43:25 -0700

    Added topic title getters and setters

--- src/ca/ualberta/cs/TopicModel.java
+++ src/ca/ualberta/cs/TopicModel.java
@@ -1,5 +1,14 @@
 package ca.ualberta.cs;
 
 public class TopicModel extends PostModel {
+	private String title;
 
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
 }
