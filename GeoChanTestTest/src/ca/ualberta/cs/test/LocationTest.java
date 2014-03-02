56089380a30ca753d7fe6e0d374d2ee2f57b1908
bvincent1 <vincent@ualberta.ca>
2014-03-02 14:21:04 -0700

    added UI picures for navigation

---
+++ GeoChanTestTest/src/ca/ualberta/cs/test/LocationTest.java
@@ -0,0 +1,13 @@
+package ca.ualberta.cs.test;
+
+import ca.ualberta.cs.MainActivity;
+import android.test.ActivityInstrumentationTestCase2;
+
+public class LocationTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+	public LocationTest() {
+		super(MainActivity.class);
+		// TODO Auto-generated constructor stub
+	}
+
+}
