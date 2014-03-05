d7fefae6b51d6d41d582913cc6fddf29bb9f94da
bvincent1 <vincent@ualberta.ca>
2014-03-05 14:30:42 -0700

    Separated classes in main activity

---
+++ src/ca/ualberta/cs/views/DummySectionFragment.java
@@ -0,0 +1,66 @@
+package ca.ualberta.cs.views;
+
+import java.util.ArrayList;
+
+import ca.ualberta.cs.R;
+import ca.ualberta.cs.controllers.PostListController;
+import ca.ualberta.cs.models.TopicModel;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
+
+/**
+ * A dummy fragment representing a section of the app, but that simply
+ * displays dummy text.
+ */
+public class DummySectionFragment extends Fragment {
+	/**
+	 * The fragment argument representing the section number for this
+	 * fragment.
+	 */
+	public static final String ARG_SECTION_NUMBER = "section_number";
+
+	public DummySectionFragment() {
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		// random generated code...
+		View rootView = inflater.inflate(R.layout.fragment_main_dummy,
+				container, false);
+		TextView dummyTextView = (TextView) rootView
+				.findViewById(R.id.section_label);
+		dummyTextView.setText(Integer.toString(getArguments().getInt(
+				ARG_SECTION_NUMBER)));
+
+		// get fragment number
+		int sectionNumber = getArguments().getInt(ARG_SECTION_NUMBER);
+
+		switch (sectionNumber) {
+			case 1:
+				populateFragment(rootView, PostListController.createTopicList());
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+		}
+
+		return rootView;
+	}
+
+	public void populateFragment(View theRootView,
+			ArrayList<TopicModel> theTopicList) {
+		ListView listView = (ListView) theRootView
+				.findViewById(R.id.postListView);
+		PostListAdapter listAdapter = new PostListAdapter(getActivity(),
+				PostListController.createTopicList());
+		listView.setAdapter(listAdapter);
+	}
+}
---
+++ src/ca/ualberta/cs/views/SectionsPagerAdapter.java
@@ -0,0 +1,57 @@
+package ca.ualberta.cs.views;
+
+import java.util.Locale;
+
+import ca.ualberta.cs.R;
+import ca.ualberta.cs.views.MainActivity.DummySectionFragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+
+/**
+ * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+	private MainActivity mainActivity;
+	
+	public SectionsPagerAdapter(MainActivity theMainActivty, FragmentManager fm) {
+		super(fm);
+		this.mainActivity = theMainActivty;
+	}
+
+	@Override
+	public Fragment getItem(int position) {
+		// getItem is called to instantiate the fragment for the given page.
+		// Return a DummySectionFragment (defined as a static inner class
+		// below) with the page number as its lone argument.
+		Fragment fragment = new DummySectionFragment();
+		Bundle args = new Bundle();
+		args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, position + 1);
+		fragment.setArguments(args);
+		return fragment;
+	}
+
+	@Override
+	public int getCount() {
+		// Show 3 total pages.
+		return 3;
+	}
+
+	@Override
+	public CharSequence getPageTitle(int position) {
+		Locale l = Locale.getDefault();
+		switch (position) {
+		case 0:
+			return this.mainActivity.getString(R.string.title_section1).toUpperCase(l);
+		case 1:
+			return this.mainActivity.getString(R.string.title_section2).toUpperCase(l);
+		case 2:
+			return this.mainActivity.getString(R.string.title_section3).toUpperCase(l);
+		}
+		return null;
+	}
+}
