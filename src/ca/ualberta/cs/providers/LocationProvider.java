7a7878ca7182ae8d0b0b23b68246800955fc7733
Wyatt Johnson <wyatt@wyattjohnson.ca>
2014-03-15 16:16:23 -0600

    Updated manifest to remove service

--- src/ca/ualberta/cs/providers/LocationProvider.java
+++ src/ca/ualberta/cs/providers/LocationProvider.java
@@ -1,14 +1,10 @@
 package ca.ualberta.cs.providers;
 
-import android.app.Service;
 import android.content.Context;
-import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.os.IBinder;
-import android.util.Log;
 
 /*
  *  Implement such as:
