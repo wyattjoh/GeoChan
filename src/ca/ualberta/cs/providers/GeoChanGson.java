b2cfc3e38ef79e2de38473c719019313b6b1fed1
Wyatt Johnson <wyatt@wyattjohnson.ca>
2014-03-23 15:10:15 -0600

    Improved serializer class

--- src/ca/ualberta/cs/providers/GeoChanGson.java
+++ src/ca/ualberta/cs/providers/GeoChanGson.java
@@ -3,6 +3,8 @@ package ca.ualberta.cs.providers;
 import java.lang.reflect.Type;
 import java.util.Date;
 
+import android.util.Base64;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonDeserializationContext;
@@ -17,30 +19,53 @@ public class GeoChanGson {
 	private static GeoChanGson singleton = null;
 	private Gson gson;
 
+	// https://gist.github.com/orip/3635246
+	// Using Android's base64 libraries. This can be replaced with any base64
+	// library.
+	private static class ByteArrayToBase64TypeAdapter implements
+			JsonSerializer<byte[]>, JsonDeserializer<byte[]> {
+		public byte[] deserialize(JsonElement json, Type typeOfT,
+				JsonDeserializationContext context) throws JsonParseException {
+			return Base64.decode(json.getAsString(), Base64.NO_WRAP);
+		}
+
+		public JsonElement serialize(byte[] src, Type typeOfSrc,
+				JsonSerializationContext context) {
+			return new JsonPrimitive(Base64.encodeToString(src, Base64.NO_WRAP));
+		}
+	}
+
+	/*
+	 * JsonSerializer<Date>, JsonDeserializer<Date>
+	 */
+	private static class DateToUnixTimestampTypeAdapter implements
+			JsonSerializer<Date>, JsonDeserializer<Date> {
+
+		@Override
+		public Date deserialize(JsonElement arg0, Type arg1,
+				JsonDeserializationContext arg2) throws JsonParseException {
+			return new Date(arg0.getAsJsonPrimitive().getAsLong());
+		}
+
+		@Override
+		public JsonElement serialize(Date arg0, Type arg1,
+				JsonSerializationContext arg2) {
+			return new JsonPrimitive(arg0.getTime());
+		}
+
+	}
+
 	private GeoChanGson() {
 		// Get builder
 		GsonBuilder gsonBuilder = new GsonBuilder();
 
-		// Register an adapter to manage the date types as long values
-		gsonBuilder.registerTypeAdapter(Date.class,
-				new JsonDeserializer<Date>() {
-					@Override
-					public Date deserialize(JsonElement arg0, Type arg1,
-							JsonDeserializationContext arg2)
-							throws JsonParseException {
-						// TODO Auto-generated method stub
-						return new Date(arg0.getAsJsonPrimitive().getAsLong());
-					}
-				});
-		
-		gsonBuilder.registerTypeAdapter(Date.class, new JsonSerializer<Date>() {
+		// Date objects
+		gsonBuilder.registerTypeHierarchyAdapter(Date.class,
+				new DateToUnixTimestampTypeAdapter());
 
-			@Override
-			public JsonElement serialize(Date arg0, Type arg1,
-					JsonSerializationContext arg2) {
-				return new JsonPrimitive(arg0.getTime());
-			}
-		});
+		// Byte objects
+		gsonBuilder.registerTypeHierarchyAdapter(byte[].class,
+				new ByteArrayToBase64TypeAdapter());
 
 		// Generate gson object
 		gson = gsonBuilder.create();
