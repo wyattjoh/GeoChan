a168d90aa98193b24240c63ced33c06066bf80ea
sarychuk <sarychuk@ualberta.ca>
2014-03-29 14:54:39 -0600

    Updated UserModel
    Added upVoteList and downVoteList

--- src/ca/ualberta/cs/models/UserModel.java
+++ src/ca/ualberta/cs/models/UserModel.java
@@ -3,6 +3,8 @@
  */
 package ca.ualberta.cs.models;
 
+import java.util.ArrayList;
+
 import android.location.Location;
 
 /**
@@ -12,7 +14,25 @@ import android.location.Location;
 public class UserModel {
 	private String userName;
 	private Location location;
+	private ArrayList<String> upVoteList;
+	private ArrayList<String> downVoteList;
 	
+	public ArrayList<String> getUpVoteList() {
+		return upVoteList;
+	}
+	
+	public void addPostIdUpVoteList(String id) {
+		upVoteList.add(id);
+	}
+	
+	public ArrayList<String> getdownVoteList() {
+		return downVoteList;
+	}
+	
+	public void addPostIdDownVoteList(String id) {
+		downVoteList.add(id);
+	}
+
 	public UserModel(String theUserName) {
 		userName = theUserName;
 	}
