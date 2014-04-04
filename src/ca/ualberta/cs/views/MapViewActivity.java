e53f6466f302ff5bda5feb2e48abc8a8d58e1b1a
TroyMurphy <tamurphy@ualberta.ca>
2014-04-04 17:51:38 -0600

    Maps are loading like a boss.

--- src/ca/ualberta/cs/views/MapViewActivity.java
+++ src/ca/ualberta/cs/views/MapViewActivity.java
@@ -1,27 +1,35 @@
 package ca.ualberta.cs.views;
 
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapController;
 import org.osmdroid.views.MapView;
 
 import android.app.Activity;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.Toast;
+import ca.ualberta.cs.R;
 
 public class MapViewActivity extends Activity {
 
+    private MapView         mMapView;
+    private MapController   mMapController;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		MapView mapView = new MapView(this, 256); //constructor
-        mapView.setClickable(true);
-        mapView.setBuiltInZoomControls(true);
-        setContentView(mapView); //displaying the MapView
-        mapView.getController().setZoom(15); //set initial zoom-level, depends on your need
-        mapView.getController().setCenter(new GeoPoint(52.221, 6.893)); //This point is in Enschede, Netherlands. You should select a point in your map or get it from user's location.
-        mapView.setUseDataConnection(false); //keeps the mapView from loading online tiles using network connection.
-
-        
+        setContentView(R.layout.activity_map_view);
+	    mMapView = (MapView) findViewById(R.id.mapview);
+	    mMapView.setTileSource(TileSourceFactory.MAPNIK);
+	    mMapView.setBuiltInZoomControls(true);
+	    mMapController = (MapController) mMapView.getController();
+	    mMapController.setZoom(13);
+	    GeoPoint gPt = new GeoPoint(51500000, -150000);
+	    //Center map near to Hyde Park Corner, London
+	    mMapController.setCenter(gPt);
+        printLocation();
     }
 
 	@Override
@@ -30,4 +38,13 @@ public class MapViewActivity extends Activity {
 		return true;
 	}
 
+	public void printLocation() {
+		Location goToLocation = (Location) getIntent().getExtras().getParcelable("postLocation");
+		try {
+			Toast.makeText(this, String.valueOf(goToLocation.getLatitude()) + " , " + 
+				String.valueOf(goToLocation.getLongitude()), Toast.LENGTH_LONG).show();
+		} catch (Exception e) {
+			Toast.makeText(this, "Failed to load location", Toast.LENGTH_SHORT).show();
+		}
+	}
 }
