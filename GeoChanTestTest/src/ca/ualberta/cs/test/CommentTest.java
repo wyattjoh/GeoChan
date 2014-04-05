d6bfb2d98e7397c25114d6db32576844cb109a2b
bvincent1 <vincent@ualberta.ca>
2014-04-05 16:37:36 -0600

    started map test class

--- GeoChanTestTest/src/ca/ualberta/cs/test/CommentTest.java
+++ GeoChanTestTest/src/ca/ualberta/cs/test/CommentTest.java
@@ -1,4 +1,6 @@
 package ca.ualberta.cs.test;
+import ca.ualberta.cs.models.CommentModel;
+import ca.ualberta.cs.models.UserModel;
 import android.app.Activity;
 import android.test.ActivityInstrumentationTestCase2;
 
@@ -8,4 +10,17 @@ public class CommentTest extends ActivityInstrumentationTestCase2<Activity> {
 		super(Activity.class);
 	}
 
+	/**
+	 * TestCase 21.1
+	 * Test whether comment takes user and allows for username retreival
+	 */
+	public void testCommentUser() {
+		String theUserName = "Testuser";
+		UserModel user = new UserModel(theUserName);
+		
+		CommentModel comment = new CommentModel();
+		comment.setPostedBy(user);
+		
+		assertEquals(theUserName, comment.getPostedBy().getUserName());
+	}
 }
