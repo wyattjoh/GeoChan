62e7fc7422bb7baa8a650fe76b8da015499a634d
TroyMurphy <tamurphy@ualberta.ca>
2014-04-04 19:30:39 -0600

    Got marker overlay working. Happy Day

--- src/ca/ualberta/cs/views/MapViewActivity.java
+++ src/ca/ualberta/cs/views/MapViewActivity.java
@@ -1,9 +1,16 @@
 package ca.ualberta.cs.views;
 
+import java.util.ArrayList;
+
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapController;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.ItemizedIconOverlay.OnItemGestureListener;
+import org.osmdroid.views.overlay.MyLocationOverlay;
+import org.osmdroid.views.overlay.OverlayItem;
+import org.osmdroid.views.overlay.ScaleBarOverlay;
 
 import android.app.Activity;
 import android.location.Location;
@@ -16,22 +23,53 @@ public class MapViewActivity extends Activity {
 
     private MapView         mMapView;
     private MapController   mMapController;
-	
+    
+    ArrayList<OverlayItem> items;
+    
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_view);
-	    mMapView = (MapView) findViewById(R.id.mapview);
-	    mMapView.setTileSource(TileSourceFactory.MAPNIK);
-	    mMapView.setBuiltInZoomControls(true);
-	    mMapController = (MapController) mMapView.getController();
-	    mMapController.setZoom(13);
-	    GeoPoint gPt = new GeoPoint(51500000, -150000);
-	    //Center map near to Hyde Park Corner, London
-	    mMapController.setCenter(gPt);
+	    this.mMapView = (MapView) findViewById(R.id.mapview);
+	    this.mMapView.setTileSource(TileSourceFactory.MAPNIK);
+	    this.mMapView.setBuiltInZoomControls(true);
+	    this.mMapController = (MapController) this.mMapView.getController();
+	    this.mMapController.setZoom(14);
+	    
+		Location goToLocation = (Location) getIntent().getExtras().getParcelable("postLocation");
+	    GeoPoint gp_postLocation = new GeoPoint(goToLocation.getLatitude(), goToLocation.getLongitude());
+	    
+	    this.mMapController.setCenter(gp_postLocation);
+	    
+	    //Add Scale Bar
+	    ArrayList<OverlayItem> theOverlayItemArray = new ArrayList<OverlayItem>();
+	    theOverlayItemArray.add(new OverlayItem(
+	    		"Post Location",
+	    		String.valueOf(gp_postLocation.getLatitude()) + " , " +String.valueOf(gp_postLocation.getLongitude()),
+	    		gp_postLocation));
+	    ItemizedIconOverlay<OverlayItem> theItemizedIconOverlay = new ItemizedIconOverlay<OverlayItem>(
+	    		this, theOverlayItemArray, myOnItemGestureListener);
+	    this.mMapView.getOverlays().add(theItemizedIconOverlay);
         printLocation();
     }
+	
+	OnItemGestureListener<OverlayItem> myOnItemGestureListener = new OnItemGestureListener<OverlayItem>() {
+		  @Override
+		  public boolean onItemLongPress(int arg0, OverlayItem arg1) {
+		   // TODO Auto-generated method stub
+		   return false;
+		  }
 
+		  @Override
+		  public boolean onItemSingleTapUp(int index, OverlayItem item) {
+		   Toast.makeText(MapViewActivity.this, 
+		     item.getTitle() + "\n"
+		     + item.getSnippet()+ "\n",
+		     Toast.LENGTH_LONG).show();
+		   return true;
+		  }
+	};
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -47,4 +85,5 @@ public class MapViewActivity extends Activity {
 			Toast.makeText(this, "Failed to load location", Toast.LENGTH_SHORT).show();
 		}
 	}
+
 }
