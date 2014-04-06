ce6fad1c3efb7d789c5e8ca5e21b63deef297830
bvincent1 <vincent@ualberta.ca>
2014-04-06 13:23:24 -0600

    Refactored dummy factory

--- src/ca/ualberta/cs/models/DummyPostListFactory.java
+++ src/ca/ualberta/cs/models/DummyPostListFactory.java
@@ -16,22 +16,8 @@ public class DummyPostListFactory {
 		// init array list
 		ArrayList<TopicModel> theModelList = new ArrayList<TopicModel>();
 
-		// populate topic with test entries
-		TopicModel theTopic1 = new TopicModel(theUser);
-		theTopic1.setTitle("TestTitle1");
-		theTopic1.setCommentText("TestText1");
-		theTopic1.setScore(1);
-		theTopic1.setPostedBy(theUser);
+		TopicModel theTopic1 = populateTestTopic(theUser);
 		theModelList.add(theTopic1);
-
-		// Create a comment
-		CommentModel theComment1 = new CommentModel();
-		theComment1.setCommentText("Test body text1");
-		theComment1.setScore(1);
-		theComment1.setPostedBy(theUser);
-
-		theTopic1.addChildComment(theComment1);
-		theComment1.setMyParent(theTopic1);
 
 		theTopicModelController.newTopic(theTopic1);
 	}
@@ -67,14 +53,18 @@ public class DummyPostListFactory {
 	public static ArrayList<CommentModel> getCommentList(UserModel theUser) {
 		ArrayList<CommentModel> theCommentList = new ArrayList<CommentModel>();
 
-		// Create a comment
-		CommentModel theComment1 = new CommentModel();
-		theComment1.setCommentText("Test body text1");
-		theComment1.setScore(1);
-		theComment1.setPostedBy(theUser);
+		CommentModel theComment1 = populateComent(theUser);
 		theCommentList.add(theComment1);
 
 		return theCommentList;
+	}
+
+	private static CommentModel populateComent(UserModel theUser) {
+		CommentModel theComment = new CommentModel();
+		theComment.setCommentText("Test body text1");
+		theComment.setScore(1);
+		theComment.setPostedBy(theUser);
+		return theComment;
 	}
 
 	/**
