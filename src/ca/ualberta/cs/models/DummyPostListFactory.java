220bbecaf91e961b3f410a4769ba586f9755a5f1
bvincent1 <vincent@ualberta.ca>
2014-04-06 13:36:18 -0600

    Revert "Refactored dummy factory"
    This reverts commit ce6fad1c3efb7d789c5e8ca5e21b63deef297830.

--- src/ca/ualberta/cs/models/DummyPostListFactory.java
+++ src/ca/ualberta/cs/models/DummyPostListFactory.java
@@ -16,8 +16,22 @@ public class DummyPostListFactory {
 		// init array list
 		ArrayList<TopicModel> theModelList = new ArrayList<TopicModel>();
 
-		TopicModel theTopic1 = populateTestTopic(theUser);
+		// populate topic with test entries
+		TopicModel theTopic1 = new TopicModel(theUser);
+		theTopic1.setTitle("TestTitle1");
+		theTopic1.setCommentText("TestText1");
+		theTopic1.setScore(1);
+		theTopic1.setPostedBy(theUser);
 		theModelList.add(theTopic1);
+
+		// Create a comment
+		CommentModel theComment1 = new CommentModel();
+		theComment1.setCommentText("Test body text1");
+		theComment1.setScore(1);
+		theComment1.setPostedBy(theUser);
+
+		theTopic1.addChildComment(theComment1);
+		theComment1.setMyParent(theTopic1);
 
 		theTopicModelController.newTopic(theTopic1);
 	}
@@ -53,18 +67,14 @@ public class DummyPostListFactory {
 	public static ArrayList<CommentModel> getCommentList(UserModel theUser) {
 		ArrayList<CommentModel> theCommentList = new ArrayList<CommentModel>();
 
-		CommentModel theComment1 = populateComent(theUser);
+		// Create a comment
+		CommentModel theComment1 = new CommentModel();
+		theComment1.setCommentText("Test body text1");
+		theComment1.setScore(1);
+		theComment1.setPostedBy(theUser);
 		theCommentList.add(theComment1);
 
 		return theCommentList;
-	}
-
-	private static CommentModel populateComent(UserModel theUser) {
-		CommentModel theComment = new CommentModel();
-		theComment.setCommentText("Test body text1");
-		theComment.setScore(1);
-		theComment.setPostedBy(theUser);
-		return theComment;
 	}
 
 	/**
