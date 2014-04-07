f7221e85ea0a6d6a66fcdecb324d05d0636ba467
vjkanhai <kanhai@ualberta.ca>
2014-04-07 08:59:01 -0600

    Stubs for all requirement 1-8 tests

--- GeoChanTestTest/src/ca/ualberta/cs/test/CommentTest.java
+++ GeoChanTestTest/src/ca/ualberta/cs/test/CommentTest.java
@@ -9,6 +9,13 @@ public class CommentTest extends ActivityInstrumentationTestCase2<Activity> {
 	public CommentTest(){
 		super(Activity.class);
 	}
+	/**
+	 * TestCase 5.1
+	 * Test whether a topic's comments are displayed
+	 */
+	public void testCommentDisplay() {
+		fail();
+	}
 	
 	/**
 	 * TestCase 6.1
@@ -23,6 +30,14 @@ public class CommentTest extends ActivityInstrumentationTestCase2<Activity> {
 	 * Test whether a new comment can be created
 	 */
 	public void testCommentCreation() {
+		fail();
+	}
+	
+	/**
+	 * TestCase 6.3
+	 * Test whether a new comment is correctly attached to its parent
+	 */
+	public void testCommentAssociation() {
 		fail();
 	}
 
